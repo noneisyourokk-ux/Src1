36674b62bfb765e52803ca28ced47a738da5eb32
noneisyourokk-ux <noneisyourokk@gmail.com>
2026-07-18 03:09:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ API_ID = os.getenv("API_ID", "22470912")
 API_HASH = os.getenv("API_HASH", "511be78079ed5d4bd4c967bc7b5ee023")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8254141632:AAFvRU6E4amexKzidBe2Ij9pBvkoCYozHNg")
 MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://rohitgolu836:zktcMCsEGQb2oIt2@cluster0.lygabfn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "76788 62761").split())) # list seperated via space
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "7678862761").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
 STRING = os.getenv("STRING", None) # optional
 LOG_GROUP = int(os.getenv("LOG_GROUP", "")) # optional with -100
